95db2bc89c1003c32655c361d64bef6ea9943a4a
yesol <maniac970430@naver.com>
2023-08-10 22:38:21 +0900

    LifeCycle: Mount, Unmount 시 동작할 코드 작성

--- src/LifeCycle.js
+++ src/LifeCycle.js
@@ -1,37 +1,23 @@
 import { useEffect, useState } from "react";
 
-const LifeCycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
-  useEffect(() => { //초기 랜딩 시
-    console.log("Mount!");
-  }, [])
-
-  useEffect(() => { //컴포넌트가 업데이트되어 상태변화가 발생할 때마다
-    console.log("Update!");
-  })
-
-  useEffect(() => { //count의 상태가 변화할 때마다
-    console.log(`count is update: ${count}`)
-    if (count > 5) {  //원하는 변수의 값이 변화하는 것을 감지하여 아래 코드를 수행 
-      alert("count가 5를 넘었습니다. 따라서 1로 초기화합니다.");
-      setCount(1);
+const UnmountTest = () => {
+  useEffect(() => {
+    console.log("Mount");
+    return () => {
+      //Unmount 시점에 실행되게 함
+      console.log("Unmount");
     }
-  }, [count])
-  
-  useEffect(() => { //text의 상태가 변화할 때마다
-    console.log(`text is update: ${text}`)
-  },[text])
+  }, [])
+  return <div>Unmount Testing Component</div>
+}
+
+const LifeCycle = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
 
   return <div style={{ padding: 20 }}>
-    <div>
-      {count}
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <div>
-        <input value={text} onChange={(e)=>setText(e.target.value)}></input>
-      </div>
-    </div>
+    <button onClick={toggle}>ON/OFF</button>
+    {isVisible && <UnmountTest />} {/*단락회로평가를 통해 뒤의 컴포넌트를 랜더링할 것인지 정하기*/}
   </div>
 };
 
