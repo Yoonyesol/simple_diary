40168c0060e996145f1f1e8a454998fa15b7fa9e
yesol <maniac970430@naver.com>
2023-08-12 21:57:24 +0900

    최적화2-2: React.memo

--- src/OptimizeTest.js
+++ src/OptimizeTest.js
@@ -1,34 +1,47 @@
 import React, { useEffect, useState } from "react";
 
-const Textview = React.memo(({text}) => {   //해당 컴포넌트(함수) 변경 사항 없을 시, 랜더링 x
-    useEffect(()=>{
-        console.log(`Update:: Text : ${text}`)
-    })
-    return <div>{text}</div>
-});
-
-const Countview = ({count}) => {    //해당 컴포넌트 변경이 없을 시에도 랜더링 발생
-    useEffect(()=>{
-        console.log(`Update:: Count : ${count}`)
+const CounterA = React.memo(({ count }) => {
+    useEffect(() => {   //memo의 동작여부를 알아보기 위해 아래 코드 실행
+        console.log(`CounterA Update - count: ${count}`)
     })
     return <div>{count}</div>
+})
+
+const CounterB = React.memo(({ obj }) => {
+    useEffect(() => {
+        console.log(`CounterB Update - count: ${obj.count}`)
+    })
+    return <div>{obj.count}</div>
+})
+
+//얕은 복사를 방지하여 최적화시키기 위한 함수
+const areEqual = (prevProps, nextProps) => {
+    //true: 이전 프롭스와 현재 프롭스가 동일하다 -> 리렌더링X
+    if (prevProps.obj.count === nextProps.obj.count) {
+        return true;
+    }
+    return false;   //이전과 현재가 다르다 -> 리렌디링O
 }
 
-const OptimizeTest = () =>{
+//Equal함수에 따라 CounterB의 리렌더링 여부가 결정되도록 하는 컴포넌트 생성
+const MemorizedCounterB = React.memo(CounterB, areEqual);
 
+const OptimizeTest = () => {
     const [count, setCount] = useState(1);
-    const [text, setText] = useState("");
+    const [obj, setObj] = useState({
+        count:1 //객체로 카운터 사용
+    });
 
     return <div style={{padding: 50}}>
         <div>
-            <h2>count</h2>
-            <Countview count={count}/>
-            <button onClick={()=>setCount(count+1)}>+</button>
+            <h2>Counter A</h2>
+            <CounterA count={count} />
+            <button onClick={()=>setCount(count)}>A button</button>{/* 상태변화 진행: count값(=1)으로 변함 */}
         </div>
         <div>
-            <h2>text</h2>
-            <Textview text={text}/>
-            <input value={text} onChange={(e)=>setText(e.target.value)}/>
+            <h2>Counter B</h2>
+            <MemorizedCounterB obj={obj}/>
+            <button onClick={()=>setObj({count: obj.count})}>B button</button>{/* 상태변화 진행 */}
         </div>
     </div>
 }
